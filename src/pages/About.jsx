fad8e54dc5d5dc0af2c67ae6c0f5fd7f5c2b7624
ahmad.al-naib <alnaib888@gmail.com>
2024-06-11 16:32:59 +0200

    add pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p>
+        This is an app to find Github users
+        
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+
+
+function NotFound() {
+  return (
+    <div>
+      <h1 className="text-6xl">Not Found</h1>
+      <p className="text-2xl">The page you are looking for does not exist</p>
+    </div>
+  )
+}
+
+export default NotFound
