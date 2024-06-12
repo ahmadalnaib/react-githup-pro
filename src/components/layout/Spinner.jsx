b9810f4107f50b46dcd1c099d858396eb499f4d8
ahmad.al-naib <alnaib888@gmail.com>
2024-06-12 16:40:44 +0200

    add spinner

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,16 @@
+import spinner from './assets/giphy.webp'
+
+function Spinner() {
+  return (
+    <div className='w-100 mt-20'>
+      <img
+      width={180}
+        src={Spinner}
+        className='text-center mx-auto'
+        alt='Loading'
+      />
+    </div>
+  )
+}
+
+export default Spinner
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,11 @@
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
+import Spinner from '../layout/Spinner'
 
 function UserResults() {
+const [users,setUsers]=useState([])
+const [loading,setLoading]=useState(true)
+
+
   useEffect(() => {
     fetchUsers()
   
@@ -9,12 +14,27 @@ function UserResults() {
   const fetchUsers = async () => {
     const res=await fetch ('https://api.github.com/users')
     const data=await res.json()
-    console.log(data)
+
+    setUsers(data)
+    setLoading(false)
+
   }
 
-  return (
-    <div>UserResults</div>
-  )
+  if(!loading){
+
+    return (
+       <div className='grid grid-cols-1 gap-8 xl:grid-col-4 lg:grid-col-4 md:grid-col-2'>
+        {users.map((user) =>(
+          <h3>{user.login}</h3>
+  
+        ))}
+  
+       </div>
+    )
+  }
+  else{
+    return <Spinner/>
+  }
 }
 
 export default UserResults
